f24af922c2ff3ac64325cc06700eddcdf0331cea
bluecube97 <82585983+bluecube97@users.noreply.github.com>
2024-03-10 19:17:26 +0900

    단어 정렬
    https://www.acmicpc.net/problem/1181
    시간초과

--- Main.java
+++ Main.java
@@ -1,37 +1,50 @@
-package bj_24_03_10;
+package bj_24_03_10_1;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int card_cnt = sc.nextInt();
-		int card_max = sc.nextInt();
+		int word_cnt = sc.nextInt();
+		List<String> word = new ArrayList<String>();
+		List<Integer> word_size = new ArrayList<Integer>();
 
-		List<Integer> card_deck = new ArrayList<Integer>();
-		for (int i = 0; i < card_cnt; i++) {
-			card_deck.add(sc.nextInt());
+		for (int i = 0; i < word_cnt; i++) {
+			word.add(sc.next());
 		}
 
-		int closeSum = 0;
+		word = word.stream().distinct().sorted().collect(Collectors.toList());
+		word_cnt = word.size();
 
-		for (int i = 0; i < card_deck.size() - 2; i++) {
-			for (int j = i + 1; j < card_deck.size() - 1; j++) {
-				for (int k = j + 1; k < card_deck.size(); k++) {
-					int result = card_deck.get(i) + card_deck.get(j) + card_deck.get(k);
+		for (int i = 0; i < word_cnt; i++) {
+			word_size.add(word.get(i).length());
+		}
 
-					if (result > closeSum && result <= card_max) {
-						closeSum = result;
-					}
+		String temp_str;
+		int temp_int;
+
+		for (int i = 0; i < word_cnt - 1; i++) {
+			for (int j = i + 1; j < word_cnt; j++) {
+				if (word_size.get(i) > word_size.get(j)) {
+					temp_str = word.get(i);
+					word.set(i, word.get(j));
+					word.set(j, temp_str);
+
+					temp_int = word_size.get(i);
+					word_size.set(i, word_size.get(j));
+					word_size.set(j, temp_int);
 				}
 			}
 		}
-		
-		System.out.println(closeSum);
+
+		for (int i = 0; i < word_cnt; i++) {
+			System.out.println(word.get(i));
+		}
 
 	}
 
