57e11e61b20ec6050a69e675e480611b0bb12bbd
bluecube97 <82585983+bluecube97@users.noreply.github.com>
2024-03-15 20:27:20 +0900

    Success!
    https://www.acmicpc.net/status?user_id=bluecube&problem_id=11050&from_mine=1
    반복문으로 해결

--- bj_24_03_15/Main.java
+++ bj_24_03_15/Main.java
@@ -14,9 +14,19 @@ public class Main {
 
     }
 
-    public static int pactorial(int n) {
-        if (n == 1) return 1;
+//    public static int pactorial(int n) {
+//        if (n == 1) return 1;
+//
+//        return n * pactorial(n - 1);
+//    }
 
-        return n * pactorial(n - 1);
+    public static int pactorial(int n) {
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
     }
 }
