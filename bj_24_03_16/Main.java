1a4bd6c3934eec96f9f60a17e397a7bdcb0d7207
bluecube97 <82585983+bluecube97@users.noreply.github.com>
2024-03-16 16:50:59 +0900

    새싹
    https://www.acmicpc.net/problem/25083

---
+++ bj_24_03_16/Main.java
@@ -0,0 +1,15 @@
+package bj_24_03_16;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |\n");
+    }
+}
