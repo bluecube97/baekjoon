9fb9344072f91e436548f498f9bfeecc14f379ae
bluecube <jph04071@gmail.com>
2024-03-19 17:52:16 +0900

    소수 구하기(미완성)
    https://www.acmicpc.net/problem/1929

---
+++ bj_24_03_19/Main.java
@@ -0,0 +1,32 @@
+package bj_24_03_19;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int M = sc.nextInt();
+//        int N = sc.nextInt();
+        List<Integer> num_list = new ArrayList<>();
+        num_list.add(2);
+
+        for (int i = 3; i < 100; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                num_list.add(i);
+            }
+        }
+
+        for (int i = 0; i < num_list.size(); i++) {
+            System.out.println(num_list.get(i));
+        }
+    }
+}
