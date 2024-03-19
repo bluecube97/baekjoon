e0ed3d2987f083ed4d3cf16cdd880fddc5a4edfe
bluecube <jph04071@gmail.com>
2024-03-19 18:13:15 +0900

    소수 구하기
    https://www.acmicpc.net/problem/1929

--- bj_24_03_19/Main.java
+++ bj_24_03_19/Main.java
@@ -7,14 +7,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        int M = sc.nextInt();
-//        int N = sc.nextInt();
+        int min = sc.nextInt();
+        int max = sc.nextInt();
         List<Integer> num_list = new ArrayList<>();
         num_list.add(2);
 
-        for (int i = 3; i < 100; i++) {
+        for (int i = 3; i < 1000000; i++) {
             boolean isPrime = true;
-            for (int j = 2; j < Math.sqrt(i); j++) {
+            for (int j = 2; j <= Math.sqrt(i); j++) {
                 if (i % j == 0) {
                     isPrime = false;
                     break;
@@ -26,7 +26,10 @@ public class Main {
         }
 
         for (int i = 0; i < num_list.size(); i++) {
-            System.out.println(num_list.get(i));
+            if (num_list.get(i) >= min && num_list.get(i) <= max){
+                System.out.println(num_list.get(i));
+            }
         }
+
     }
 }
