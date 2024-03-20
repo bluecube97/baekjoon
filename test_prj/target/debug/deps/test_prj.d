497f62292028cc0cecda39a65a2341d988971fe5
bluecube <jph04071@gmail.com>
2024-03-20 13:20:59 +0900

    del test

--- test_prj/target/debug/deps/test_prj.d
+++
@@ -1,5 +0,0 @@
-C:\Users\502\RustroverProjects\test_prj\target\debug\deps\test_prj.exe: src/testdir/main.rs
-
-C:\Users\502\RustroverProjects\test_prj\target\debug\deps\test_prj.d: src/testdir/main.rs
-
-src/testdir/main.rs:
--- test_prj/target/debug/test_prj.d
+++
@@ -1,1 +0,0 @@
-C:\Users\502\RustroverProjects\test_prj\target\debug\test_prj.exe: C:\Users\502\RustroverProjects\test_prj\src\testdir\main.rs
