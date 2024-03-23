f4f2689d1e4e6a4653c5350c1896554f497e431f
bluecube97 <82585983+bluecube97@users.noreply.github.com>
2024-03-24 04:52:12 +0900

    개
    https://www.acmicpc.net/problem/10172

---
+++ bj_24_03_24/Main.java
@@ -0,0 +1,11 @@
+package bj_24_03_24;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
