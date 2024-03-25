647b8e9cb6f96b831a2423703dab9da03b7eb7ef
bluecube <jph04071@gmail.com>
2024-03-25 18:18:58 +0900

    Hashing
    https://www.acmicpc.net/problem/15829

---
+++ bj_24_03_25/Main.java
@@ -0,0 +1,14 @@
+package bj_24_03_25;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int length = sc.nextInt();
+        String str = sc.next();
+
+        
+    }
+}
