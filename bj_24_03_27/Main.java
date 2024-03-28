5bd9837e50b6b3b36fda7b9db4d2d344493c2f25
bluecube <jph04071@gmail.com>
2024-03-28 18:20:35 +0900

    Update Main.java

--- bj_24_03_27/Main.java
+++ bj_24_03_27/Main.java
@@ -1,27 +1,31 @@
 package bj_24_03_27;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
+// https://inpa.tistory.com/entry/JAVA-%E2%98%95-LinkedList-%EA%B5%AC%EC%A1%B0-%EC%82%AC%EC%9A%A9%EB%B2%95-%EC%99%84%EB%B2%BD-%EC%A0%95%EB%B3%B5%ED%95%98%EA%B8%B0
+// https://www.acmicpc.net/problem/11866
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//      https://www.acmicpc.net/problem/11866
+
         int people = sc.nextInt();
         int cnt = sc.nextInt();
-        List<Integer> list = new ArrayList<>();
+
+        LinkedList<Integer> list = new LinkedList<>();
+        LinkedList<Integer> resultList = new LinkedList<>();
 
         for (int i = 0; i < people; i++) {
-            list.add(i);
+            list.addLast(i + 1);
         }
 
-        for (int i = 0; i < people; i++) {
-            int j = cnt;
-
-
+        for (int i = cnt; resultList.size() == people; i += cnt) {
+            while (cnt > list.size()) {
+                cnt -= list.size();
+            }
+            resultList.add(list.get(cnt));
+            list.remove(cnt);
         }
 
+        System.out.println(resultList);
     }
 }
