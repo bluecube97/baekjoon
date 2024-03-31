6b62cedcdcb337a0e69df55fdaf9bdf283c7ab63
bluecube97 <82585983+bluecube97@users.noreply.github.com>
2024-03-31 21:44:25 +0900

    N 찍기
    https://www.acmicpc.net/problem/2741

--- bj_24_03_30_2/Main.java
+++ bj_24_03_30_2/Main.java
@@ -6,5 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int input = sc.nextInt();
+
+        for (int i = 0; i < input; i++) {
+            System.out.println(i + 1);
+        }
+
     }
 }
