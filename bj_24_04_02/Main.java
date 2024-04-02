64ac7ff50a708b05ecb872b0b662b6e2b51c3e4a
bluecube <jph04071@gmail.com>
2024-04-02 18:18:56 +0900

    체스판 다시 칠하기
    https://www.acmicpc.net/problem/1018

--- bj_24_04_02/Main.java
+++ bj_24_04_02/Main.java
@@ -34,6 +34,5 @@ public class Main {
                 System.out.println("YES");
             }
         }
-
     }
 }
---
+++ bj_24_04_02_1/Main.java
@@ -0,0 +1,23 @@
+package bj_24_04_02_1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        List<List<Integer>> numList = new ArrayList<>();
+        List<Integer> innerNumList = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                
+            }
+        }
+    }
+}
