6cc7bfdff20888c711f5a9aedaaae55d850f6d77
bluecube97 <82585983+bluecube97@users.noreply.github.com>
2024-04-03 22:23:53 +0900

    체스판 다시 칠하기
    https://www.acmicpc.net/problem/1018

--- bj_24_04_02_1/Main.java
+++ bj_24_04_02_1/Main.java
@@ -1,23 +1,55 @@
 package bj_24_04_02_1;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static boolean[][] arr;
+    public static int min = 64;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        List<List<Integer>> numList = new ArrayList<>();
-        List<Integer> innerNumList = new ArrayList<>();
+        arr = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
+            String str = sc.next();
             for (int j = 0; j < M; j++) {
-                
+                arr[i][j] = str.charAt(j) == 'W';
             }
         }
+
+        int N_row = N - 7;
+        int M_col = M - 7;
+
+        for (int i = 0; i < N_row; i++) {
+            for (int j = 0; j < M_col; j++) {
+                find(i, j);
+            }
+        }
+        System.out.println(min);
+    }
+
+    private static void find(int x, int y) {
+        int end_x = x + 8;
+        int end_y = y + 8;
+        int cnt = 0;
+
+        boolean TF = arr[x][y];
+
+        for (int i = x; i < end_x; i++) {
+            for (int j = y; j < end_y; j++) {
+                if (arr[i][j] != TF){
+                    cnt++;
+                }
+
+                TF = !TF;
+            }
+            TF = !TF;
+        }
+
+        cnt = Math.min(cnt, 64 - cnt);
+        min = Math.min(min, cnt);
     }
 }
