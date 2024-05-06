40e2e33bd34f81a34540986d2703cacf81d3ba8f
bluecube97 <jph04071@gmail.com>
2024-05-07 01:28:48 +0900

    시험 성적
    https://www.acmicpc.net/problem/9498

---
+++ bj_24_05_07/Main.java
@@ -0,0 +1,32 @@
+package bj_24_05_07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+
+        switch (input / 10) {
+            case 10:
+                System.out.println("A");
+                break;
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+        }
+    }
+}
