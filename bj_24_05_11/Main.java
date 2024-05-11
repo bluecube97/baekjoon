272e2e494ee7660d086ecc091ca0e8ebe14c0069
bluecube97 <jph04071@gmail.com>
2024-05-12 05:00:05 +0900

    긴자리 계산
    https://www.acmicpc.net/problem/2338

---
+++ bj_24_05_11/Main.java
@@ -0,0 +1,19 @@
+package bj_24_05_11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BigInteger a = new BigInteger(br.readLine());
+        BigInteger b = new BigInteger(br.readLine());
+
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.println(a.multiply(b));
+    }
+}
