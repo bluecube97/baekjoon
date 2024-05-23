83de34986ec8f8217e1210c3626201a642958d6f
bluecube97 <jph04071@gmail.com>
2024-05-23 23:26:14 +0900

    숫자 카드
    https://www.acmicpc.net/problem/10815

--- bj_24_05_23/Main.java
+++ bj_24_05_23/Main.java
@@ -3,9 +3,7 @@ package bj_24_05_23;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
