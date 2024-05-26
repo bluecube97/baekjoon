bd085477f9fc7240bf6863390be787b2d9cfc066
bluecube97 <jph04071@gmail.com>
2024-05-27 01:35:15 +0900

    킹, 퀸, 룩, 비숍, 나이트, 폰
    https://www.acmicpc.net/problem/3003

---
+++ bj_24_05_26/Main.java
@@ -0,0 +1,22 @@
+package bj_24_05_26;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int king = Integer.parseInt(st.nextToken());
+        int queen = Integer.parseInt(st.nextToken());
+        int rook = Integer.parseInt(st.nextToken());
+        int bishop = Integer.parseInt(st.nextToken());
+        int knight = Integer.parseInt(st.nextToken());
+        int pawn = Integer.parseInt(st.nextToken());
+
+        System.out.println((1 - king) + " " + (1 - queen) + " " + (2 - rook) + " " + (2 - bishop) + " " + (2 - knight) + " " + (8 - pawn));
+    }
+}
