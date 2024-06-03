6940fc6563719da25652450f4d6bd4459265bb73
bluecube97 <jph04071@gmail.com>
2024-06-03 20:40:03 +0900

    인사성 밝은 곰곰이
    https://www.acmicpc.net/problem/25192

--- bj_24_06_03/Main.java
+++ bj_24_06_03/Main.java
@@ -3,20 +3,24 @@ package bj_24_06_03;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int chatCnt = Integer.parseInt(br.readLine());
-        boolean isEnter = false;
         int result = 0;
-        HashMap<String, Integer> chatMap = new HashMap<>();
+        Map<String, Boolean> chatMap = new HashMap<>();
 
         for (int i = 0; i < chatCnt; i++) {
-
+            String chat = br.readLine();
+            if (chat.equals("ENTER")) {
+                chatMap.clear();
+            } else if (!chatMap.getOrDefault(chat, false)) {
+                result++;
+                chatMap.put(chat, true);
+            }
         }
 
         System.out.println(result);
