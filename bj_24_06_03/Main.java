f5e93acb0a7e3c553972ca44270b323206abd20a
bluecube <jph04071@gmail.com>
2024-06-03 18:10:52 +0900

    인사성 밝은 곰곰이
    https://www.acmicpc.net/problem/25192

---
+++ bj_24_06_03/Main.java
@@ -0,0 +1,24 @@
+package bj_24_06_03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int chatCnt = Integer.parseInt(br.readLine());
+        boolean isEnter = false;
+        int result = 0;
+        HashMap<String, Integer> chatMap = new HashMap<>();
+
+        for (int i = 0; i < chatCnt; i++) {
+
+        }
+
+        System.out.println(result);
+    }
+}
