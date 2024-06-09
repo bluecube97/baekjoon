f64770fd9d24520e83703f34ad0260223acb4655
bluecube97 <jph04071@gmail.com>
2024-06-10 01:45:42 +0900

    엄청난 부자2
    https://www.acmicpc.net/problem/1271

---
+++ bj_24_06_09/Main.java
@@ -0,0 +1,20 @@
+package bj_24_06_09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+    }
+}
