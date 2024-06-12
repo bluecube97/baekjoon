7274bb5fff0866286e70bd219eab8d5835c9877e
bluecube97 <jph04071@gmail.com>
2024-06-12 21:41:15 +0900

    큐 2
    https://www.acmicpc.net/problem/18258

---
+++ bj_24_06_12/Main.java
@@ -0,0 +1,60 @@
+package bj_24_06_12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine());
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < testcase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String order = st.nextToken();
+            switch (order) {
+                case "push":
+                    int num = Integer.parseInt(st.nextToken());
+                    queue.add(num);
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.poll()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "front":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.peek()).append("\n");
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.peekLast()).append("\n");
+                    }
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
