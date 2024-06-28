a7d602b49b7cd001680b6dcb92a55c48309cbfe8
bluecube <jph04071@gmail.com>
2024-06-28 18:20:10 +0900

    핸드폰 번호 궁합
    https://www.acmicpc.net/problem/17202

---
+++ bj_24_06_28/Main.java
@@ -0,0 +1,29 @@
+package bj_24_06_28;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String phoneA = br.readLine();
+        String phoneB = br.readLine();
+        String result = "";
+
+        for (int i = 0; i < phoneA.length(); i++) {
+            result += phoneA.charAt(i);
+            result += phoneB.charAt(i);
+        }
+
+        while (result.length() < 3) {
+            String tempResult = "";
+            for (int i = 0; i < result.length() - 1; i++) {
+                int num = Integer.parseInt(result.substring(i, i + 1)) + Integer.parseInt(result.substring(i + 1, i + 2));
+                if (num >= 10) {
+                    num -= 10;
+                }
+            }
+        }
+    }
+}
