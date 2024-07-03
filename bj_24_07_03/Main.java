3ed1b4e886ebbce9d9c46956b0a09bf2a57c6350
bluecube <jph04071@gmail.com>
2024-07-03 18:19:33 +0900

    정보보호학부 동아리 소개
    https://www.acmicpc.net/problem/28691

---
+++ bj_24_07_03/Main.java
@@ -0,0 +1,30 @@
+package bj_24_07_03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        switch (input) {
+            case "M":
+                System.out.println("MatKor");
+                break;
+            case "W":
+                System.out.println("WiCys");
+                break;
+            case "C":
+                System.out.println("CyKor");
+                break;
+            case "A":
+                System.out.println("AlKor");
+                break;
+            case "$":
+                System.out.println("$clear");
+                break;
+        }
+    }
+}
---
+++ bj_24_07_03_1/Main.java
@@ -0,0 +1,11 @@
+package bj_24_07_03_1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
