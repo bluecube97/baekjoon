658c4ab3f48a155806fc1bfdc2bf7be2abe6cb4c
bluecube <jph04071@gmail.com>
2024-07-16 17:56:03 +0900

    수학숙제
    https://www.acmicpc.net/problem/2870

--- bj_24_07_15/Main.java
+++ bj_24_07_15/Main.java
@@ -3,22 +3,42 @@ package bj_24_07_15;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigInteger;
+import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        List<Integer> numList = new ArrayList<>();
+        PriorityQueue<BigInteger> numPq = new PriorityQueue<>();
+        String numStr = "";
 
         for (int i = 0; i < N; i++) {
+            if (!numStr.isEmpty()) {
+                numPq.add(new BigInteger(numStr));
+                numStr = "";
+            }
+
             String str = br.readLine();
+
             for (char c : str.toCharArray()) {
-                if (c > 96 && c < 123) {
-                    numList.add(c - 65);
+                if (c > 47 && c < 58) {
+                    numStr += c;
+                } else if (!numStr.isEmpty()) {
+                    numPq.add(new BigInteger(numStr));
+                    numStr = "";
                 }
             }
+        }
+
+        if (!numStr.isEmpty()) {
+            numPq.add(new BigInteger(numStr));
+        }
+
+        int numPqSize = numPq.size();
+
+        for (int i = 0; i < numPqSize; i++) {
+            System.out.println(numPq.poll());
         }
     }
 }
