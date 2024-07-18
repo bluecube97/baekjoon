369a081e060ca537db97def9049b59a96a6bd9bf
bluecube <jph04071@gmail.com>
2024-07-18 18:19:57 +0900

    소수 단어
    https://www.acmicpc.net/problem/2153

---
+++ bj_24_07_18_2/Main.java
@@ -0,0 +1,35 @@
+package bj_24_07_18_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        int wordNum = 0;
+
+        for (char c : word.toCharArray()) {
+            if (c > 64 && c < 91)
+                wordNum += c - 64 + 27;
+            else if (c > 96 && c < 123)
+                wordNum += c - 96;
+        }
+
+        if (isPrime(wordNum))
+            System.out.println("It is a prime word.");
+        else
+            System.out.println("It is not a prime word.");
+    }
+
+    private static boolean isPrime(int wordNum) {
+        if (wordNum == 1)
+            return true;
+        for (int i = 2; i < Math.sqrt(wordNum); i++) {
+            if (wordNum % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
