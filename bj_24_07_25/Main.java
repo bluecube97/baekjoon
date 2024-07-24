415a998b13609b4b2e3c778b7a42d677fa810120
bluecube97 <jph04071@gmail.com>
2024-07-25 00:37:39 +0900

    선린인터넷고등학교 교가
    https://www.acmicpc.net/problem/21964

---
+++ bj_24_07_25/Main.java
@@ -0,0 +1,14 @@
+package bj_24_07_25;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        System.out.println(str.substring(N - 5, N));
+    }
+}
