ef44467016c39de871f9f2765f443772a644c794
bluecube97 <jph04071@gmail.com>
2024-08-02 20:33:39 +0900

    다리 놓기
    https://www.acmicpc.net/problem/1010

---
+++ bj_24_08_02/Main.java
@@ -0,0 +1,34 @@
+package bj_24_08_02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < testcase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            System.out.println(combination(M, N));
+        }
+
+    }
+
+    public static long combination(int n, int r) {
+        if (r > n - r) {
+            r = n - r;
+        }
+        long result = 1;
+        for (int i = 0; i < r; i++) {
+            result *= (n - i);
+            result /= (i + 1);
+        }
+        return result;
+    }
+}
