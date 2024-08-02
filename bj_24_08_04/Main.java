1593a97c0591fb52fed6fa46ec510cedb758ee15
bluecube97 <jph04071@gmail.com>
2024-08-02 20:34:02 +0900

    Moo
    https://www.acmicpc.net/problem/1809

---
+++ bj_24_08_04/Main.java
@@ -0,0 +1,12 @@
+package bj_24_08_04;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("(___)\n" +
+                "(o o)____/\n" +
+                " @@      \\\n" +
+                "  \\ ____,/\n" +
+                "  //   //\n" +
+                " ^^   ^^");
+    }
+}
