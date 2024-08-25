5c6df2b1c9ae0380ac7be1741a29d8ae3b04778f
bluecube97 <jph04071@gmail.com>
2024-08-26 01:58:06 +0900

    주사위
    https://www.acmicpc.net/problem/9295

---
+++ bj_24_08_25/Main.java
@@ -0,0 +1,20 @@
+package bj_24_08_25;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < testcase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println("Case " + (i + 1) + ": " + (a + b));
+        }
+    }
+}
