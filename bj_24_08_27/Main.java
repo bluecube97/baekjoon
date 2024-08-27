8e34fdfb701a335041f90528635344c1cb2214c0
bluecube97 <jph04071@gmail.com>
2024-08-28 01:42:20 +0900

    모음의 개수
    https://www.acmicpc.net/problem/1264

---
+++ bj_24_08_27/Main.java
@@ -0,0 +1,27 @@
+package bj_24_08_27;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String str = br.readLine();
+            if (str.equals("#")) {
+                break;
+            }
+
+            int count = 0;
+
+            for (char c : str.toCharArray()) {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
