0ca2ebbfc831b7c9bf944b6c614626d6f568b1e4
bluecube97 <jph04071@gmail.com>
2024-09-05 11:19:05 +0900

    별 찍기 - 12
    https://www.acmicpc.net/problem/2522

---
+++ bj_24_09_05/Main.java
@@ -0,0 +1,31 @@
+package bj_24_09_05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = N - i - 1; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < N - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
