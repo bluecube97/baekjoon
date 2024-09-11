3ec72a85b3d33f6b46bd40ae6bba24a4b6d5795d
bluecube97 <jph04071@gmail.com>
2024-09-12 03:30:34 +0900

    직사각형
    https://www.acmicpc.net/problem/27323

---
+++ bj_24_09_11/Main.java
@@ -0,0 +1,14 @@
+package bj_24_09_11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(A * B);
+    }
+}
