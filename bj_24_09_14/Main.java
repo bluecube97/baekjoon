e1479595a022159dfbacd88ce416c5940c60c382
bluecube97 <jph04071@gmail.com>
2024-09-14 21:30:51 +0900

    럭비 클럽
    https://www.acmicpc.net/problem/2083

---
+++ bj_24_09_14/Main.java
@@ -0,0 +1,29 @@
+package bj_24_09_14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            if (name.equals("#") && age == 0 && weight == 0) {
+                break;
+            }
+
+            if (age > 17 || weight >= 80) {
+                System.out.println(name + " Senior");
+            } else {
+                System.out.println(name + " Junior");
+            }
+        }
+    }
+}
