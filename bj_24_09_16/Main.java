96625635aaedec133b65d032fe3cfc1fde5bede9
bluecube97 <jph04071@gmail.com>
2024-09-16 11:53:52 +0900

    파티가 끝나고 난 뒤
    https://www.acmicpc.net/problem/2845

---
+++ bj_24_09_16/Main.java
@@ -0,0 +1,24 @@
+package bj_24_09_16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int L = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int result = L * P;
+        for (int i = 0; i < 5; i++) {
+            System.out.print(arr[i] - result + " ");
+        }
+    }
+}
