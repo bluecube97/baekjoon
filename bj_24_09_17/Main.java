f97a4269ab0c3112b973a9ab03e95d9eab874653
bluecube97 <jph04071@gmail.com>
2024-09-20 02:08:54 +0900

    크냐?
    https://www.acmicpc.net/problem/4101

---
+++ bj_24_09_17/Main.java
@@ -0,0 +1,28 @@
+package bj_24_09_17;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (A == 0 && B == 0) {
+                break;
+            }
+
+            if (A > B) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
