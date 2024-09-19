6d0f4aa2696b58dbda8f6f70fa7ac25199f4971b
bluecube97 <jph04071@gmail.com>
2024-09-20 02:11:06 +0900

    주사위 세개
    https://www.acmicpc.net/problem/2480

---
+++ bj_24_09_19/Main.java
@@ -0,0 +1,27 @@
+package bj_24_09_19;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        if (A == B && B == C) {
+            System.out.println(10000 + A * 1000);
+        } else if (A == B || A == C) {
+            System.out.println(1000 + A * 100);
+        } else if (B == C) {
+            System.out.println(1000 + B * 100);
+        } else {
+            System.out.println(Math.max(A, Math.max(B, C)) * 100);
+        }
+    }
+}
