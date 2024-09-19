ba85f5ba768378de7b9f49fb94b65635292b53bc
bluecube97 <jph04071@gmail.com>
2024-09-20 02:09:04 +0900

    아!
    https://www.acmicpc.net/problem/4999

---
+++ bj_24_09_18/Main.java
@@ -0,0 +1,19 @@
+package bj_24_09_18;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String can = br.readLine();
+        String doctor = br.readLine();
+
+        if (can.length() >= doctor.length()) {
+            System.out.println("go");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
