1a67aff3f163257c141bb75f1e89eee4ff76d1bd
bluecube97 <jph04071@gmail.com>
2024-10-03 00:07:00 +0900

    헛간 청약
    https://www.acmicpc.net/problem/19698

---
+++ bj_24_10_02/Main.java
@@ -0,0 +1,24 @@
+package bj_24_10_02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int result = (W / L) * (H / L);
+        if (result > N) {
+            System.out.println(N);
+        } else {
+            System.out.println(result);
+        }
+    }
+}
