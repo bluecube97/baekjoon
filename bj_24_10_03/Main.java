3ed09707eefba39921b8b307adda0cf38f17c76b
bluecube97 <jph04071@gmail.com>
2024-10-04 04:33:41 +0900

    Previous Level
    https://www.acmicpc.net/problem/28453

---
+++ bj_24_10_03/Main.java
@@ -0,0 +1,27 @@
+package bj_24_10_03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            if (input == 300) {
+                System.out.print(1 + " ");
+            } else if (input >= 275) {
+                System.out.print(2 + " ");
+            } else if (input >= 250) {
+                System.out.print(3 + " ");
+            } else {
+                System.out.print(4 + " ");
+            }
+        }
+    }
+}
