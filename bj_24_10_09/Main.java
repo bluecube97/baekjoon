91d534075aa9e1672e0d1342cdb281075dc2501d
bluecube97 <jph04071@gmail.com>
2024-10-09 23:47:22 +0900

    초코바
    https://www.acmicpc.net/problem/27959

---
+++ bj_24_10_09/Main.java
@@ -0,0 +1,19 @@
+package bj_24_10_09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if (N * 100 >= M) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
