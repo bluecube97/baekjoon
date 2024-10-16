f86c4c04de816c703ee5b96301fc9ccfe5b4aafc
bluecube97 <jph04071@gmail.com>
2024-10-17 00:51:03 +0900

    콜센터
    https://www.acmicpc.net/problem/5339

---
+++ bj_24_10_16/Main.java
@@ -0,0 +1,16 @@
+package bj_24_10_16;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
